ec10582db6d2c832c4cef99fd3f7280d7e823912
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-06-27 21:12:42 +0300

    ...

--- src/components/Importer.tsx
+++ src/components/Importer.tsx
@@ -4,13 +4,17 @@ export let obj: any;
 export async function Importer() {
   const link = "https://api.artic.edu/api/v1/artworks?limit=100";
     let data;
-    fetch(link, requestOptions)
-    .then(res=>res.json())
-        .then(json=> {
-            data = json
-            console.log(`this is json rendered from importer ${json}`);
 
-        })
-    return data;
+    var response = await fetch(link, requestOptions).catch((err) => { console.error(err); });
+console.log(response)
 }
 
+// 
+//await fetch(link, requestOptions)
+//.then(res=>res.json())
+  //  .then(json=> {
+    //    data = json
+      //  console.log(`this is json rendered //from importer ${json}`);
+
+    //})
+//return data;
