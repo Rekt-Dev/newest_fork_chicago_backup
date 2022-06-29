44332cde1159efedbae82e0bde6f25706bf94092
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-06-29 21:41:25 +0300

    ...

--- src/services/importer.tsx
+++ src/services/importer.tsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import { requestOptions } from "../services/requestOptions";
 export let obj: any;
 
 export function importer() {
   const link = "https://api.artic.edu/api/v1/artworks?limit=100";
 
-  fetch(link, requestOptions)
-    .then((res) => res.json())
-    .then((data) => (obj = data))
-    .then(() => console.log(obj));
-  console.log(`this is obj rendered from importer ${obj}`);
+  const [data, setData] = useState("");
+  async function getData() {
+    try {
+      const response = await fetch(link);
+      setTodos(await response.json());
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  }
 }
