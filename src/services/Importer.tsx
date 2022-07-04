7d5d3f7c773455686a0a993192264a55d93a630f
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-07-04 21:31:34 +0300

    ...

--- src/services/Importer.tsx
+++ src/services/Importer.tsx
@@ -4,12 +4,12 @@ import { useState } from "react";
 export function Importer() {
   const link = "https://api.artic.edu/api/v1/artworks?limit=100";
   let data1;
-  let data2
+  let data2;
   const [data, setData] = useState("");
   async function getData() {
     try {
       const response = await fetch(link);
-      setData( response.json());
+      setData(response.json());
       data1 = data;
       console.log(`this is response ${response}`);
       console.log(`this is data ${data}`);
@@ -18,5 +18,5 @@ export function Importer() {
     }
     getData();
   }
-  return <div> setData</div>;
+  return <div> {data}</div>;
 }
