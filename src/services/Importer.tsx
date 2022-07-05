5761671eac5078913255c3d357746a9bfdf4854d
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-07-05 07:03:08 +0300

    ...

--- src/services/Importer.tsx
+++ src/services/Importer.tsx
@@ -5,11 +5,11 @@ export function Importer() {
   const link = "https://api.artic.edu/api/v1/artworks?limit=20";
   let data1;
   let data2;
-  const [data, setData] = useState("");
+  const [data, setData] = useState([]);
   async function getData() {
     try {
       const response = await fetch(link);
-      setData(response.json());
+      setData(await response.json());
       data1 = data;
       console.log(`this is response ${response}`);
       console.log(`this is data ${data}`);
